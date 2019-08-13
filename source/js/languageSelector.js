270a135fcab4f1b09c3dd27b6544f766e3982b22
Marvin Dalheimer <me@marvin-dalheimer.de>
2019-08-13 08:44:06 +0200

    Add body listener for trigger closing language box

--- source/js/languageSelector.js
+++ source/js/languageSelector.js
@@ -5,6 +5,12 @@ if(!isMobile()) {
   languageSelector = document.getElementsByClassName("language-selector-actual")[0];
   moreLanguagesContainer = document.getElementsByClassName("language-selector-more")[0];
 
+  document.body.addEventListener("click", () => {
+    if (moreLanguagesContainer && !moreLanguagesContainer.classList.contains("hidden")) {
+      moreLanguagesContainer.classList.add("hidden");
+    }
+  });
+
   languageSelector && languageSelector.addEventListener("click", e => {
     e.stopPropagation();
     moreLanguagesContainer.classList.toggle("hidden");
