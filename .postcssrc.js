58bfba3985a8a1dd4fc8d65c6564f5f715ce92ad
panr <radoslaw.koziel@gmail.com>
2021-03-21 09:59:10 +0100

    [chore] update dependencies and modernize postcss config

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,7 +2,6 @@ const url = require("postcss-url");
 const imports = require("postcss-import");
 const nested = require("postcss-nested");
 const postCSSPresetEnv = require("postcss-preset-env");
-const browsers = require("browserslist");
 const cssnano = require("cssnano");
 const color = require("postcss-color-mod-function");
 const mixins = require("postcss-mixins");
