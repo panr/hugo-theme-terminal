8b1529f4bba9e3ba28b10006e43d58d78ec29e82
panr <radoslaw.koziel@gmail.com>
2021-10-24 18:17:10 +0200

    clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
-const Webpack = require("webpack");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 const path = require("path");
